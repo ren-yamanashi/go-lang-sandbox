9d0dbdee52b030da89ae8e0cecc907c4d8fc80cb
ren-yamanashi <pinokionigg@icloud.com>
2025-12-28 20:23:04 +0900

    fmt

--- src/local/mypkg/mypkg.go
+++ src/local/mypkg/mypkg.go
@@ -2,10 +2,10 @@ package mypkg
 
 import "fmt"
 
-func FuncA() {			// 大文字で始まるものは自動的にエクスポートされる
-    fmt.Println("Run FuncA!")
+func FuncA() { // 大文字で始まるものは自動的にエクスポートされる
+	fmt.Println("Run FuncA!")
 }
 
-func funcB() {			// 小文字で始まるものはエクスポートされない
-    fmt.Println("Run funcB!")
+func funcB() { // 小文字で始まるものはエクスポートされない
+	fmt.Println("Run funcB!")
 }
